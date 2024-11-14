88ff4f9bcbb2375434629f305917c2f6b420bc52
DiegoFaduli <diegofaduli@gmail.com>
2024-11-13 22:48:34 -0300

    teste-rota

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,26 +1,14 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import './App.css'
+import Footer from './Components/Footer.jsx'
+import Header from './Components/Header.jsx'
+import { Outlet } from 'react-router-dom'
 
 function App() {
 
   return (
     <>
-      <Router>
-        <div className="dark flex flex-col min-h-screen">
-          <div className="flex flex-grow">
-            <Navbar />
-            <main className="flex-grow p-4">
-              <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/about" element={<About />} />
-                <Route path="/projects" element={<Projects />} />
-                <Route path="/contact" element={<Contact />} />
-              </Routes> 
-            </main>
-          </div>
-          <Footer />
-        </div>
-      </Router>
+      <Header />
+      <Outlet />
+      <Footer/>
     </>
   )
 }
---
+++ vite-project/src/pages/Simulacao.jsx
@@ -0,0 +1,7 @@
+export default function Simulacao() {
+    return(
+        <>
+            <h1>Simulacao</h1>
+        </>
+    )
+}
