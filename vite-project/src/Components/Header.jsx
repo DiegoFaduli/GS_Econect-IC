03975205b95973cd893f4fb43281b580e0cbfcb6
DiegoFaduli <diegofaduli@gmail.com>
2024-11-15 17:29:47 -0300

    arrumando

--- vite-project/src/Components/Header.jsx
+++ vite-project/src/Components/Header.jsx
@@ -5,12 +5,10 @@ export default function Header() {
   return (
     <nav className="w-full p-5 bg-white shadow-md flex justify-center items-center">
       <div className="flex items-center justify-center w-full max-w-7xl space-x-8">
-        {/* Logo */}
         <div className="flex items-center space-x-2">
           <img className="w-28 h-auto mr-36" src={Econect} alt="Logo Econect" />
         </div>
         
-        {/* Navigation Links */}
         <div className="text-lg flex space-x-8 text-gray-800">
           <Link to="/" className="hover:text-green-500 font-bold">Home</Link>
           <Link to="/sobre" className="hover:text-green-500 font-bold">Sobre</Link>
@@ -18,7 +16,6 @@ export default function Header() {
           <Link to="/servicos" className="hover:text-green-500 font-bold">Serviços</Link>
         </div>
         
-        {/* Botão "Contato" */}
         <Link to="/contato" className="bg-green-500 text-white px-4 py-2 rounded-md hover:bg-green-600 ">
           Contato
         </Link>
